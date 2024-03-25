c0b676a5b940634fe9c9a924beffda340ab9f209
DexLovecraft <andrewlondon16@gmail.com>
2024-03-25 21:13:29 +0100

    semantic in html , change of class and id for BEM name, small styling update, and change of name in js

--- test.js
+++ test.js
@@ -1,41 +1,15 @@
-let Temperature = {
-    0: 15,
-    500 : 11.8,
-    1000 : 8.5,
-    1500 : 5.3,
-    2000 : 2.0,
-    2500 : -1.3,
-    3000: -4.5,
-    3500: -7.8, 
-    4000: -11.0,
-    4500: -14.3,
-    5000: -17.5,
-    5500: -20.8,
-    6000: -24.0,
-    6500: -27.3,
-    7000: -30.5,
-    7500: -33.8,
-    8000: -37.0,
-    8500: -40.3,
-    9000: -43.5,
-    9500: -46.8,
-    10000: -50.0,
-    10500: -53.3,
-    11000: -56.5,
-}
+let Temperature = {0: 15, 500 : 11.8, 1000 : 8.5, 1500 : 5.3, 2000 : 2.0, 2500 : -1.3, 3000: -4.5, 3500: -7.8, 4000: -11.0, 4500: -14.3, 5000: -17.5, 5500: -20.8, 6000: -24.0, 6500: -27.3, 7000: -30.5, 7500: -33.8, 8000: -37.0, 8500: -40.3, 9000: -43.5, 9500: -46.8, 10000: -50.0, 10500: -53.3,11000: -56.5}
 
-const speedMetricInput = document.getElementById('SpeedMectric')
-const speedKnotsInput = document.getElementById('SpeedKnots')
-const altitudeMetricInput = document.getElementById('altitudeMetric')
-const altitudeFootInput = document.getElementById('altitudeFoot')
-const calculateButton = document.getElementById('Calculate')
-const machInput = document.getElementById('Mach')
+const speedMetricInput = document.getElementById('speed_form_metric')
+const speedKnotsInput = document.getElementById('speed_form_knots')
+const altitudeMetricInput = document.getElementById('altitude_form_metric')
+const altitudeFootInput = document.getElementById('altitude_form_foot')
+const machInput = document.getElementById('mach')
 let dataTemperature = 0
 let speed = 1.944
 
 const temperatureConversion = (AltitudeUser) => {
     let keys = 0
-
     if (AltitudeUser > 11000) {
         keys = 11000
     }
@@ -65,13 +39,6 @@ const altitudeConversion = (e, type) => {
     temperatureConversion(altitudeMeter)
 }
 
-const machOperation = (temperature) => {
-    let machValue = 0
-    machValue = speed/(39*Math.sqrt(temperature))
-    machValue = machValue.toFixed(2)
-    machInput.value = machValue
-}
-
 const speedConversion = (e, type) => {
     if (type == "knots") {
         speed = e.target.value
@@ -82,6 +49,13 @@ const speedConversion = (e, type) => {
         speedKnotsInput.value = speed.toFixed(0)
     }
     machOperation(dataTemperature)
+}
+
+const machOperation = (temperature) => {
+    let machValue = 0
+    machValue = speed/(39*Math.sqrt(temperature))
+    machValue = machValue.toFixed(2)
+    machInput.value = machValue
 }
 
 const speedOperation = (e, temperature) => {
